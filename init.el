d710a74ebf01239311ff0b741f4f0edff6daa41a
hectorgrey <hectorgrey223@protonmail.com>
2024-03-14 17:44:28 +0000

    Removed backup files

--- init.el
+++ init.el
@@ -4,9 +4,9 @@
 
 (setq use-dialog-box nil)
 (setq inhibit-startup-message t)
+(setq make-backup-files nil)
 (tool-bar-mode -1)
 (tooltip-mode -1)
-(set-fringe-mode 10)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 (save-place-mode 1)
