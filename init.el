18afc61649ca7ec2f5b4811148a53071ee2e274b
hectorgrey <hectorgrey223@protonmail.com>
2024-03-14 16:04:57 +0000

    Moved custom variables to a separate file.

--- init.el
+++ init.el
@@ -7,8 +7,11 @@
 (tooltip-mode -1)
 (set-fringe-mode 10)
 (scroll-bar-mode -1)
-
 (menu-bar-mode -1)
+(save-place-mode 1)
+
+(setq custom-file (locate-user-emacs-file "custom-vars.el"))
+(load custom-file 'noerror 'nomessage)
 
 (load-theme 'modus-vivendi)
 
@@ -43,16 +46,3 @@
 	 ("C-j" . ivy-next-line)
 	 ("C-d" . ivy-reverse-i-search-kill)))
 (ivy-mode 1)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(ivy evil magit)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
